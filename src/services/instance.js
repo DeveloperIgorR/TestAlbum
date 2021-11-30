d5318d53c8ca7ac02390ed986e56423c42a47974
DeveloperIgorR <bratca@inbox.ru>
2021-11-30 02:41:56 +0200

    album fix2

--- src/services/instance.js
+++ src/services/instance.js
@@ -2,6 +2,7 @@ import * as axios from 'axios'
 
 export const SERVER_URL = 'https://picsum.photos/v2/list?page=2&limit=100'
 
-export const instance = axios.create({      
+export const instance = axios.create({ 
+    headers: { 'Content-Type': 'application/json' },
     baseURL: SERVER_URL     
 })
